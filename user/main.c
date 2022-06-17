eeaf643517393b8a3040e011a3d385b94737d6b0
SevenD <enter775@gmail.com>
2022-06-17 16:50:36 +0800

    带头双向链表的插入，尾部插入，中间插入，删除实验

--- user/main.c
+++ user/main.c
@@ -1,19 +1,82 @@
+/*
+*************************************************************************
+*                             包含的头文件
+*************************************************************************
+*/
 #include "list.h"
 
-List_t list_root;
+/*
+*************************************************************************
+*                              全局变量
+*************************************************************************
+*/
+
+/* 定义链表根节点 */
+struct xLIST       List_Test;
+
+/* 定义节点 */
+struct xLIST_ITEM  List_Item1;
+struct xLIST_ITEM  List_Item2;
+struct xLIST_ITEM  List_Item3;
+struct xLIST_ITEM  List_Item4;
+
+int remain_num;
 
 
-
+/*
+************************************************************************
+*                                main函数
+************************************************************************
+*/
+/*
+* 注意事项：1、该工程使用软件仿真，debug需选择 Ude Simulator
+*           2、在Target选项卡里面把晶振Xtal(Mhz)的值改为25，默认是12，
+*              改成25是为了跟system_ARMCM3.c中定义的__SYSTEM_CLOCK相同，确保仿真的时候时钟一致
+*/
 int main(void)
-{
-	vListInitialise(&list_root);
-
-	for(;;)
+{	
+	
+    /* 链表根节点初始化 */
+    vListInitialise( &List_Test );
+    
+    /* 节点1初始化 */
+    vListInitialiseItem( &List_Item1 );
+    List_Item1.xItemValue = 1;
+    
+    /* 节点2初始化 */    
+    vListInitialiseItem( &List_Item2 );
+    List_Item2.xItemValue = 2;
+    
+    /* 节点3初始化 */
+    vListInitialiseItem( &List_Item3 );
+    List_Item3.xItemValue = 3;
+	
+	/* 节点4初始化 */
+    vListInitialiseItem( &List_Item4 );
+    List_Item3.xItemValue = 4;
+    
+    /* 将节点插入链表，按照升序排列 */
+    vListInsert( &List_Test, &List_Item1 );    
+    vListInsert( &List_Test, &List_Item3 );  
+	/* 链表中间插入实验  */
+    vListInsert( &List_Test, &List_Item2 );	
+	
+	/* 尾部插入 实验  */
+    vListInsertEnd( &List_Test, &List_Item4 );
+	
+	remain_num = uxListRemove(&List_Item2);
+	
+	if(remain_num == 3)
 	{
+		/* 调试标识，如果进入循环，则删除成功 */
+		while(1){}
 		
 	}
-
-	
-	
+    
+    for(;;)
+	{
+		/* 啥事不干 */
+	}
 }
+
 
